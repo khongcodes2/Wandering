1088f340291a693d82b4b1c04c53d44a0d70348b
KHong <albtrossfacefall@gmail.com>
2019-11-24 12:18:35 -0500

    deleted old comment about journey has_many spaces through regions

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -3,7 +3,6 @@ class Journey < ActiveRecord::Base
     belongs_to :traveler
     belongs_to :region
     has_many :space_journeys
-    # no longer has_many through regions - all of the region's spaces became the journey's
     has_many :spaces, through: :space_journeys
     has_many :item_journeys
     has_many :items, through: :item_journeys
